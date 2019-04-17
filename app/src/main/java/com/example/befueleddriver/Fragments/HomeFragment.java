f9bedabdb324150d1362eb3db66540272c0abd19
Faizan Shahid <faizanshahid0334@gmail.com>
2019-04-17 12:47:48 +0500

    second commit

--- app/src/main/java/com/example/befueleddriver/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/befueleddriver/Fragments/HomeFragment.java
@@ -2,11 +2,9 @@ package com.example.befueleddriver.Fragments;
 
 import android.Manifest;
 import android.animation.ObjectAnimator;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -18,10 +16,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
-import com.example.befueleddriver.Adapters.UserRecyclerAdapter;
+import com.example.befueleddriver.Adapters.RecyclerViewAdapter;
 import com.example.befueleddriver.Models.CustomerRequest;
 import com.example.befueleddriver.R;
 import com.example.befueleddriver.Utils.ViewWeightAnimationWrapper;
@@ -29,7 +27,6 @@ import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
 import com.firebase.geofire.GeoQuery;
 import com.firebase.geofire.GeoQueryEventListener;
-import com.firebase.geofire.LocationCallback;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -40,7 +37,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -52,7 +48,6 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-import static android.support.constraint.Constraints.TAG;
 import static com.example.befueleddriver.Utils.Constants.MAPVIEW_BUNDLE_KEY;
 
 
@@ -60,28 +55,41 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
         GoogleApiClient.OnConnectionFailedListener, LocationListener, View.OnClickListener, com.google.android.gms.location.LocationListener {
 
     private static final String TAG = "HomeFragment";
+    private static final int MAP_LAYOUT_STATE_CONTRACTED = 0;
+    private static final int MAP_LAYOUT_STATE_EXPANDED = 1;
+    private static final int LIMIT = 1;
+    final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
     LocationRequest mLocationRequest;
-    private MapView mMapView;
-    private GoogleMap mMap;
     FirebaseAuth mAuth;
     DatabaseReference myRef;
     ArrayList<String> username, email;
-    ArrayList<CustomerRequest> customerRequests;
 
-    private UserRecyclerAdapter adapter;
+    boolean isDriverFound = false;
+    private MapView mMapView;
+    private GoogleMap mMap;
+    private RecyclerViewAdapter adapter;
     private String userID;
     private RecyclerView recyclerView;
     private RelativeLayout mMapContainer;
     private LatLng latLng;
     private FusedLocationProviderClient mFusedLocationClient;
-
-
-    private static final int MAP_LAYOUT_STATE_CONTRACTED = 0;
-    private static final int MAP_LAYOUT_STATE_EXPANDED = 1;
     private int mMapLayoutState = 0;
+    private int radius = 1;
+    private int distance = 1;
+    private String customerid;
+    private ArrayList<String> mkey = new ArrayList<>();
+    private String mlocalkey;
+    private int i = 0;
 
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -97,42 +105,47 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
 
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         mAuth = FirebaseAuth.getInstance();
+
         userID = mAuth.getCurrentUser().getUid();
         mMapView = view.findViewById(R.id.user_list_map);
         mMapContainer = view.findViewById(R.id.map_container);
-        Log.d(TAG, "onCreateViewuserid: "+userID);
+        Log.d(TAG, "onCreateViewuserid: " + userID);
         view.findViewById(R.id.btn_full_screen_map).setOnClickListener(this);
+
         myRef = FirebaseDatabase.getInstance().getReference().child("driverAvailable").child(userID);
         // Inflate the layout for this fragment
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
         username = new ArrayList<>();
         email = new ArrayList<>();
+//        loadAllCustomers();
         initRecyclerView(view);
         initGoogleMap(savedInstanceState);
         return view;
     }
 
     private void initRecyclerView(View view) {
-        username.add("faizan");
-        email.add("faizan@gmail.com");
-
-        username.add("shahid");
-        email.add("shahid@gmail.com");
-
-        username.add("sheh");
-        email.add("sheh@gmail.com");
-
-
-
-
         Log.d(TAG, "initRecyclerView: ");
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("customerRequest");
         recyclerView = view.findViewById(R.id.user_list_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        ref.addValueEventListener(new ValueEventListener() {
+        ref.child("customerRequestInfo").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ArrayList<CustomerRequest> list = new ArrayList<>();
+                dataSnapshot.getKey();
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    i++;
+                    CustomerRequest mRequest = dataSnapshot1.getValue(CustomerRequest.class);
+                    if (mkey.contains(mRequest.getUserID()) && mRequest.isIsorderplaced()) {
+                        list.add(mRequest);
+                        Log.d(TAG, "onDataChange:datasnapshot " + " " + mRequest.getCarId() + " " + mRequest.getUserID());
+                        Log.d(TAG, "onDataChange:datasnapshot" + mRequest);
+                        Log.d(TAG, "onDataChange:datasnapshot" + "---------------");
+                    }
 
+
+                }
+                adapter = new RecyclerViewAdapter(getActivity(), list, getActivity().getSupportFragmentManager());
+                recyclerView.setAdapter(adapter);
             }
 
             @Override
@@ -141,8 +154,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
             }
         });
 
-        adapter = new UserRecyclerAdapter(getActivity(), username, email);
-        recyclerView.setAdapter(adapter);
+
     }
 
     private void initGoogleMap(Bundle savedInstanceState) {
@@ -154,14 +166,6 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
         mMapView.onCreate(mapViewBundle);
 
         mMapView.getMapAsync(this);
-    }
-
-    public static HomeFragment newInstance() {
-        return new HomeFragment();
-    }
-
-    public HomeFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -229,13 +233,13 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
         }
         mMap.setMyLocationEnabled(true);
 
-//        mFusedLocationClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
-//            @Override
-//            public void onSuccess(Location location) {
-//                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
-//                mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
-//            }
-//        });
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+            }
+        });
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -247,10 +251,6 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
         mGoogleApiClient.connect();
 
     }
-
-
-
-
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
@@ -279,9 +279,9 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
     @Override
     public void onLocationChanged(Location location) {
         mLastLocation = location;
-        latLng = new LatLng(location.getLatitude(),location.getLongitude());
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+        latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
         mMap.clear();
         loadAllCustomers();
 //        mMap.addMarker(new MarkerOptions().position(latLng).title("Right Now"));
@@ -316,43 +316,47 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_full_screen_map:{
+        switch (v.getId()) {
+            case R.id.btn_full_screen_map: {
 
-                if(mMapLayoutState == MAP_LAYOUT_STATE_CONTRACTED){
+                if (mMapLayoutState == MAP_LAYOUT_STATE_CONTRACTED) {
                     mMapLayoutState = MAP_LAYOUT_STATE_EXPANDED;
                     expandMapAnimation();
-                }
-                else if(mMapLayoutState == MAP_LAYOUT_STATE_EXPANDED){
+                } else if (mMapLayoutState == MAP_LAYOUT_STATE_EXPANDED) {
                     mMapLayoutState = MAP_LAYOUT_STATE_CONTRACTED;
                     contractMapAnimation();
                 }
+
                 break;
             }
 
         }
     }
 
-    private int radius=1;
-    private int distance=1;
-    boolean isDriverFound = false;
-    private String customerid;
-    private static final int LIMIT = 3;
-
-    private void loadAllCustomers(){
+    private void loadAllCustomers() {
         DatabaseReference customerLocation = FirebaseDatabase.getInstance().getReference().child("customerRequest");
-        Log.d(TAG, "loadAllCustomersss: "+mLastLocation);
+        Log.d(TAG, "loadAllCustomersss: " + mLastLocation);
         GeoFire geoFire = new GeoFire(customerLocation);
-        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(mLastLocation.getLatitude(),mLastLocation.getLongitude()),distance);
+        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude()), distance);
+//        mMap.clear();
         geoQuery.removeAllListeners();
         geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
             @Override
-            public void onKeyEntered(String key, final GeoLocation location) {
-                Log.d(TAG, "onKeyEntered: "+ key);
+            public void onKeyEntered(final String key, final GeoLocation location) {
+                Log.d(TAG, "onKeyEntered: " + key);
                 FirebaseDatabase.getInstance().getReference("customerRequest").child(key)
                         .addListenerForSingleValueEvent(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                mlocalkey = key;
+                                Log.d(TAG, "onDataChange:load " + mlocalkey);
+                                if (!mkey.contains(mlocalkey)) {
+                                    mkey.add(mlocalkey);
+//                                    initRecyclerView(getView());
+                                    Log.d(TAG, "onDataChange:s " + mkey);
+
+                                }
                                 mMap.addMarker(new MarkerOptions().
                                         position(new LatLng(location.latitude,location.longitude))
                                         .flat(true)
@@ -378,8 +382,9 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
 
             @Override
             public void onGeoQueryReady() {
-                if (distance<=LIMIT){
+                if (distance <= LIMIT) {
                     distance++;
+                    Log.d(TAG, "onGeoQueryReady: " + mlocalkey);
                     loadAllCustomers();
                 }
             }
@@ -451,7 +456,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
 ////        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(latLng))
 //    }
 
-    private void expandMapAnimation(){
+    private void expandMapAnimation() {
         ViewWeightAnimationWrapper mapAnimationWrapper = new ViewWeightAnimationWrapper(mMapContainer);
         ObjectAnimator mapAnimation = ObjectAnimator.ofFloat(mapAnimationWrapper,
                 "weight",
@@ -470,7 +475,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
         mapAnimation.start();
     }
 
-    private void contractMapAnimation(){
+    private void contractMapAnimation() {
         ViewWeightAnimationWrapper mapAnimationWrapper = new ViewWeightAnimationWrapper(mMapContainer);
         ObjectAnimator mapAnimation = ObjectAnimator.ofFloat(mapAnimationWrapper,
                 "weight",
